c8f2150b32e2ee91b25ede9f7f59c641206ca139
Ali Home <rezatrue@yahoo.com>
2022-03-07 10:07:04 +0600

    Setup & first run

---
+++ src/main/java/App.java
@@ -0,0 +1,16 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class App extends Base{
+
+	public static void main(String[] args) throws MalformedURLException {
+		System.out.println("Welcome to Appium Ecommerce Demo");
+		AndroidDriver<AndroidElement> driver = capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	}
+
+}
